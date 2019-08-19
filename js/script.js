2f832f0e0d61f0da3186d5b7bb346e1498421f66
Michał Nabielec <michal.nabie@gmail.com>
2019-08-19 13:17:42 +0200

    Restrict field mark (unable to overwrite exist mark)

--- js/script.js
+++ js/script.js
@@ -3,24 +3,31 @@ let use_x = true;
 
 function handleCellOnClick() {
     let r_index, c_index;
-    //GET CLICKED ROW/COLUMN PARAMNETERS
+    //GET CLICKED ROW/COLUMN PARAMETERS
     for (let r = 0; r<game_board.rows.length; r++){
         for (let c = 0; c<game_board.rows[r].cells.length; c++){
             // return
             game_board.rows[r].cells[c].onclick = function () {
                 r_index = this.parentElement.rowIndex;
                 c_index = this.cellIndex;
-                console.log("R/C", [r_index, c_index]);
-                markCell(r_index,c_index);
+                //console.log("R/C", [r_index, c_index]);
+                checkCell(r_index,c_index);
             }
         }
     }
 }
 
 function markCell(r_index, c_index){
-    let cell = game_board.rows[r_index].cells[c_index].innerHTML = use_x ? "X" : "0";
+    game_board.rows[r_index].cells[c_index].innerHTML = use_x ? "X" : "0";
     use_x = !use_x;
 
+}
+
+function checkCell(r_index, c_index) {
+    console.log(game_board.rows[r_index].cells[c_index].innerHTML);
+    if(game_board.rows[r_index].cells[c_index].innerHTML === "a"){
+        markCell(r_index,c_index);
+    }
 }
 
 // let variables;
