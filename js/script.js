d4495c06ff01a51a4fd9ece2c01ee8942c2e6921
Michał Nabielec <michal.nabie@gmail.com>
2019-08-20 11:46:35 +0200

    Program check winning condition (add columns condition)

--- js/script.js
+++ js/script.js
@@ -25,7 +25,7 @@ function markCell(r_index, c_index) {
 
 function checkCell(r_index, c_index) {
     console.log(game_board.rows[r_index].cells[c_index].innerHTML);
-    if (game_board.rows[r_index].cells[c_index].innerHTML === "a") {
+    if (game_board.rows[r_index].cells[c_index].innerHTML === "") {
         markCell(r_index, c_index);
         checkWinConditions();
     }
@@ -99,6 +99,32 @@ function checkDiagonal() {
     }
 }
 
+function checkColumns() {
+    let player1 = 0;
+    let player2 = 0;
+
+    for (let column = 0; column < game_board.rows.length; column++) {
+        for (let itemIndex = 0; itemIndex < game_board.rows.length; itemIndex++) {
+            if (game_board.rows[itemIndex].cells[column].innerHTML === 'X') {
+                player1 += 1;
+            }
+            if (game_board.rows[itemIndex].cells[column].innerHTML === 'O') {
+                player2 += 1;
+            }
+        }
+
+        if (player1 === 3) {
+            return 1;
+        }
+        if (player2 === 3) {
+            return 2;
+        }
+        player1 = 0;
+        player2 = 0;
+
+    }
+}
+
 function checkWinConditions() {
     console.log('test');
     if (checkRows() === 1) {
@@ -112,12 +138,15 @@ function checkWinConditions() {
     } else if (checkDiagonal() === 2) {
         console.log('PLAYER 2 win')
     }
+
+    if (checkColumns() === 1) {
+        console.log('PLAYER 1 win')
+    } else if (checkColumns() === 2) {
+        console.log('PLAYER 2 win')
+    }
+
+
 }
 
-// let variables;
 handleCellOnClick();
-checkDiagonal();
 
-// console.log(variables);
-
-// getTableRowAndColumnFromClick();
