eda2c401479d336be7233a354df94050915e0ea4
Michał Nabielec <michal.nabie@gmail.com>
2019-08-20 12:09:25 +0200

    Start clean code

--- js/script.js
+++ js/script.js
@@ -3,14 +3,12 @@ let use_x = true;
 
 function handleCellOnClick() {
     let r_index, c_index;
-    //GET CLICKED ROW/COLUMN PARAMETERS
     for (let r = 0; r < game_board.rows.length; r++) {
         for (let c = 0; c < game_board.rows[r].cells.length; c++) {
             // return
             game_board.rows[r].cells[c].onclick = function () {
                 r_index = this.parentElement.rowIndex;
                 c_index = this.cellIndex;
-                //console.log("R/C", [r_index, c_index]);
                 checkCell(r_index, c_index);
             }
         }
@@ -168,6 +166,5 @@ function removeAL() {
         }
     }
 }
-
 handleCellOnClick();
 
